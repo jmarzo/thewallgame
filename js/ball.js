93c5d948d80ac423acdbfa0443bc86afbcd1f872
Jacopo Marzola <marzola.jacopo@gmail.com>
2023-06-13 15:31:25 +0200

    added rebound, first sounds, racket control

--- js/ball.js
+++ js/ball.js
@@ -1,8 +1,78 @@
 class Ball {
-  constructor(x, y, speed, direction) {
-    this.x = x;
-    this.y = y;
+  constructor(speed, racket) {
+    this.element = document.getElementById("ball"); // getting the div ball
+    this.x = this.element.getBoundingClientRect().left;
+    this.y = this.element.getBoundingClientRect().top;
+    this.direction = { dx: 0, dy: 1 }; // default to get into the while
     this.speed = speed;
-    this.direction = direction;
+    this.racket = racket;
+    this.racketHit = new Audio("../sounds/mixkit-metal-bowl-hit-1842.wav");
+    this.sadTrombone = new Audio("../sounds/Sad-trombone.mp3");
+    this.topBottomHit = new Audio(
+      "../sounds/mixkit-metallic-sword-strike-2160.wav"
+    );
+
+    this.reset(); // execute the reset method to restart from the default setting
   }
+
+  reset() {
+    //nitial setting -----> ball default position
+    this.element.style.left = "10vw";
+    this.element.style.top = "50vh";
+
+    //direction
+    while (
+      Math.abs(this.direction.dx <= 0.2) ||
+      Math.abs(this.direction.dx >= 0.9)
+    ) {
+      const angle = getRandom(0, 2 * Math.PI); // random between 0° and 360°
+
+      console.log(angle);
+
+      this.direction.dx = Math.cos(angle);
+      this.direction.dy = Math.sin(angle);
+    }
+    //console.log(this.direction);
+  }
+
+  update(timeSlice) {
+    const ballBound = this.element.getBoundingClientRect(); //to manage ball collisions
+
+    const racketBound = this.racket.element.getBoundingClientRect();
+
+    if (ballBound.top <= 0 || ballBound.bottom >= window.innerHeight) {
+      this.direction.dy *= -1; //invert y vector
+      this.racketHit.play();
+      this.y += this.direction.dy * this.speed * timeSlice;
+    }
+
+    if (
+      ballBound.right >= window.innerWidth ||
+      (ballBound.left <= racketBound.right &&
+        ballBound.top >= racketBound.top - ballBound.height &&
+        ballBound.bottom <= racketBound.bottom + ballBound.height) // the ball can bounce only on the racket
+    ) {
+      this.direction.dx *= -1; // invert x vector
+      this.racketHit.play();
+      this.x += this.direction.dx * this.speed * timeSlice;
+    }
+
+    if (ballBound.left <= 0) {
+      this.element.style.display = "none";
+      this.sadTrombone.play();
+      //location.reload();
+    } else {
+      this.x += this.direction.dx * this.speed * timeSlice;
+      this.y += this.direction.dy * this.speed * timeSlice;
+    }
+
+    //console.log(this.x);
+
+    this.element.style.top = `${this.y}px`;
+    this.element.style.left = `${this.x}px`;
+  }
+}
+
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
 }
--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 let previousTime;
 
+const racket = new Racket();
+const ball = new Ball(0.4, racket);
+
 function update(time) {
   if (previousTime) {
     const timeSlice = time - previousTime;
-    console.log(timeSlice);
+    //console.log(timeSlice);
+    //update the ball
+    ball.update(timeSlice);
   }
 
   previousTime = time;
@@ -11,4 +16,17 @@ function update(time) {
   window.requestAnimationFrame(update);
 }
 
-window.requestAnimationFrame(update);
+const onMouseMove = (e) => {
+  racket.element.style.top = e.pageY + "px";
+};
+
+let startButton = document.getElementById("start-button");
+
+document.addEventListener("mousemove", onMouseMove); // you can move the racket
+
+startButton.addEventListener("click", () => {
+  let startDiv = document.getElementById("game-start");
+  startDiv.style.display = "none";
+
+  window.requestAnimationFrame(update);
+});
